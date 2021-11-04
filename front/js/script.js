e5032e77bff51d6e8a67a98ae58f2354da5ae563
Samuel Lego <samuelego44@gmail.com>
2021-11-04 10:47:04 +0100

    etape 6 en cours

---
+++ front/js/script.js
@@ -0,0 +1,44 @@
+// URL du produit
+let productsURL = 'http://localhost:3000/api/products/';
+
+//  ETAPE 6 : Insérer un produit et ses détails dans la page
+Produit
+function create_Product (elements) {
+    const kanap = document.create_Element ('');  //document  : https://developer.mozilla.org/fr/docs/Web/API/Document/Document
+    kanap.href = 'http://localhost:3000/api/products/' + productId;
+
+
+
+    const product_article  = document.createElement ('');        // const article
+
+    const product_name = document.createElement ('');           // const name
+    product_name
+
+
+    const product_picture = document.createElement ('');         // const imageUrl
+    product_picture.src
+
+
+
+    const product_descrption = document.createElement ('');     // const description
+    product_descrption
+
+
+
+
+    
+    
+    
+    
+}
+
+//  ETAPE 5 : Récupérer l’id du produit à afﬁcher
+
+
+
+
+
+// extraction ID
+let productId = "107fb5b75607497b96722bda5b504926" ;
+let create_item_page_url = 'http://localhost:3000/api/products/'+ productId
+console.log (create_item_page_url);
