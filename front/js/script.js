78cb17a85166a5fbbad87fe38dbe7ad13428fbf4
Samuel Lego <samuelego44@gmail.com>
2021-11-09 10:39:34 +0100

    amelioration et remplacement var par const

--- front/js/script.js
+++ front/js/script.js
@@ -3,9 +3,8 @@ let productsURL = "http://localhost:3000/api/products/";
 
 //  ETAPE 6 : Insérer un produit et ses détails dans la page
 
- // Création élément (produit)
+// Création élément (produit)
 function create_Product(elements) {
- 
   const kanap = document.create_Element("a"); // https://developer.mozilla.org/fr/docs/Web/API/Document/Document
   kanap.href = "http://localhost:3000/api/products/" + productId;
 
@@ -35,14 +34,14 @@ let productId = "107fb5b75607497b96722bda5b504926";
 let create_item_page_url = "http://localhost:3000/api/products/" + productId;
 console.log(create_item_page_url);
 
-function Data (URL) { // https://developer.mozilla.org/fr/docs/Web/API/URL
-fetch (URL) // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
-.then(function(response) {
-  if(response.ok) {
-    return response//???
-    else {
-      console.error('erreur') //https://developer.mozilla.org/fr/docs/Web/API/Console/error
-    }
-  }
-}
+function Data(URL) {
+  // https://developer.mozilla.org/fr/docs/Web/API/URL
+  fetch(URL) // https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error("erreur"); //https://developer.mozilla.org/fr/docs/Web/API/Console/error
+      }
+    });
 }
