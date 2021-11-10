ad44ed556d3968c271773eb511babca9a9587cb6
Samuel Lego <samuelego44@gmail.com>
2021-11-10 12:49:29 +0100

    refonte globale

--- front/js/products.js
+++ front/js/products.js
@@ -1,15 +1,64 @@
-//  ETAPE 5 : Récupérer l’id du produit à afﬁcher
+// ETAPE 6
 
-// URL API du produit
-let productsURL = "http://localhost:3000/api/products/";
+// URL de API d'un produit
+let productsURL = "http://localhost:3000/api/products/" + product_id;
+
+// extraction ID
+let productId = "107fb5b75607497b96722bda5b504926";
+let create_item_page_url = "http://localhost:3000/api/products/" + productId;
+console.log(create_item_page_url);
+
+// API
+function FetchItem(create_item_page_url) {
+  // https://developer.mozilla.org/fr/docs/Web/API/URL
+  fetch(create_item_page_url)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.log("mauvaise réponse du réseau"); //https://developer.mozilla.org/fr/docs/Web/API/Console/error
+      }
+    })
+    .catch(function (error) {
+      console.log("problème avec fetch");
+    });
+}
+
+// Création élément (produit)
+function create_Product(elements) {
+  const kanap = document.create_Element("a"); // https://developer.mozilla.org/fr/docs/Web/API/Document/Document
+  kanap.href = "http://localhost:3000/api/products/" + productId;
+
+  // Création constructor article, name, image et description
+  const product_article = document.createElement("article"); // const article
+
+  const product_name = document.createElement("h3"); // const name
+  product_name.innerHTML = elements.name; // insertion lien HTML
+  product_name.classList.add("productName"); // ajout class productName
+
+  const product_picture = document.createElement("img"); // const imageUrl
+  product_picture.src = elements.ImageUrl;
+  product_picture.alt = elements.Alt;
+
+  const product_descrption = document.createElement("p"); // const description
+  product_descrption.innerHTML = elements.description;
+  product_descrption.classList.add("productDescription"); // ajout class productDescription
+
+  // Page produit (DOM)
+  product_article.appendChild(product_name);
+  product_article.appendChild(product_picture);
+  product_article.appendChild(product_descrption);
+}
 
 // Récuperation via API des informations produits
-const title = document.getElementById("title");
-const price = document.getElementById("price");
-const description = document.getElementById("description");
-const colorDescription = document.getElementById("colors");
+var title = document.getElementById("title");
+var price = document.getElementById("price");
+var description = document.getElementById("description");
+var colorDescription = document.getElementById("colors");
 
-// Récuperaton ID dans l'URl
+// ETAPE 5
 
-const urlSearchParams = URLSearchParams;
-const productId = urlSearchParams.getElementById("id");
+// Récupération ID de l'url de la page produit
+var paramsString = location.search;
+var searchParams = new URLSearchParams(paramsString);
+var product_id = searchParams.get("product_id");
