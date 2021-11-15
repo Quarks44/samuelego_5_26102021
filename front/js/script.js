61cf0dd5ce702ec6a2ab490938693fdac429e419
Samuel Lego <samuelego44@gmail.com>
2021-11-15 10:50:50 +0100

    ajout fletchproduct

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,17 @@
-// ETAPE 4
+//API
+
+function FetchProduct () {
+    fetch('http://localhost:3000/api/products')
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          console.log("mauvaise réponse du réseau"); 
+        }
+      })
+      .catch(function (error) {
+        console.log("problème avec fetch");
+      });
+  }//end function
 
 
