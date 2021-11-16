06c0acc96580b4d7a503eaa33f67ce1d5f757cc8
Samuel Lego <samuelego44@gmail.com>
2021-11-16 17:15:19 +0100

    ajout plusieurs elements

--- front/js/script.js
+++ front/js/script.js
@@ -11,16 +11,47 @@ async function fetchApi() {
     });
 } // end function
 
-// Affichage plusieurs produits
-//function displayKanaps(sofas) {}
+//function displayKanaps(products) {}
 //let  = document.getElementById("items");
 //end function
 
+// Affichage plusieurs produits
+function displayAllKanaps(listofAllKanaps) {
+  //  lien  fiche produit
+  const sofaId = document.createElement("a");
+  sofaId.href = "product.html?id=" + productId;
+
+  // Création Article
+  const article = document.createElement("article");
+
+  // Création Images
+  const newProductPhoto = document.createElement("img");
+  newProductPhoto.src = listofAllKanaps.imageUrl;
+  newProductPhoto.alt = listofAllKanaps.altTxt;
+
+  // Création Name
+  const newProductTitle = document.createElement("h3");
+  newProductTitle.innerHTML = listofAllKanaps.name;
+  newProductTitle.classList.add("productName");
+
+  // Création Description
+  const newProductDescription = document.createElement("p");
+  newProductDescription.innerHTML = listofAllKanaps.description;
+  newProductDescription.classList.add("productDescription");
+
+  // Ajout des éléments
+  article.appendChild(newProductPhoto);
+  article.appendChild(newProductTitle);
+  article.appendChild(newProductDescription);
+  sofaId.appendChild(article);
+  document.getElementById("items").appendChild(sofaId);
+} // end function
+
 // Principale Fonction
 async function main() {
-  let sofa = await fetchApi(); // = API
-  for (let element of sofa) {
-    displayKanap(sofa); // affichage
+  let listofAllKanaps = await fetchApi(); // = API
+  for (let element of listofAllKanaps) {
+    displayKanap(listofAllKanaps); // affichage
   }
 }
 main();
