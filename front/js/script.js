fbb1bde5c3ae88a2ed7cfdded78a0c798fe10ce8
Samuel Lego <samuelego44@gmail.com>
2021-11-16 11:06:30 +0100

    refonte globale et changement de nom

--- front/js/script.js
+++ front/js/script.js
@@ -1,17 +1,26 @@
-//API
+//  API
+async function fetchApi() {
+  return fetch("http://localhost:3000/api/products/")
+    .then(function (res) {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+} // end function
 
-function FetchProduct () {
-    fetch('http://localhost:3000/api/products')
-      .then(function (response) {
-        if (response.ok) {
-          return response.json();
-        } else {
-          console.log("mauvaise réponse du réseau"); 
-        }
-      })
-      .catch(function (error) {
-        console.log("problème avec fetch");
-      });
-  }//end function
+// Affichage plusieurs produits
+//function displayKanaps(sofas) {}
+//let  = document.getElementById("items");
+//end function
 
-
+// Principale Fonction
+async function main() {
+  let sofa = await fetchApi(); // = API
+  for (let element of sofa) {
+    displayKanap(sofa); // affichage
+  }
+}
+main();
