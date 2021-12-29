7133ae4d16acdf2214e4714cd07df8983f07469c
Samuel Lego <samuelego44@gmail.com>
2021-12-29 13:10:33 +0100

    ajout message lors du passage de commande

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,4 +1,5 @@
 const params = new URL(document.location).searchParams;
 const orderId = params.get("orderId");
-
+console.log(orderId);
 document.getElementById("orderId").textContent = orderId;
+alert("Commande effectuée avec succès");
